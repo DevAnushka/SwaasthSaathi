f68731dfa6d51e8fc4e383700cfaac0e66091517
DevAnushka <143649387+DevAnushka@users.noreply.github.com>
2026-09-18 00:42:19 +0530

    Add frontend/next.config.mjs

---
+++ frontend/next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
